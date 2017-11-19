ec072b9f90c78630bd1ca89c8ab69809b630f6d4
Nicholas Dujay <nickdujay@gmail.com>
2017-11-18 22:25:32 -0500

    snapshot

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,37 @@
-#[macro_use]
-extern crate error_chain;
+extern crate failure;
+extern crate ncollide;
 extern crate piston_window;
 extern crate tiled;
 
+use failure::Error;
 use piston_window::*;
-use std::fs::File;
 
-mod errors;
+mod view;
 
-fn run() -> errors::Result<()> {
-  let mut window: PistonWindow = WindowSettings::new("prototype", [960, 640]).exit_on_esc(true)
-    .build()?;
+fn run() -> Result<(), Error> {
+  let mut window: PistonWindow = WindowSettings::new("prototype", [960, 640])
+    .exit_on_esc(true)
+    .build()
+    .unwrap();
 
-  let reader = File::open("assets/main.tmx")?;
-  let map = tiled::parse(reader)?;
-
-  let tileset = map.get_tileset_by_gid(1).cloned().unwrap();
-  let tile_width = tileset.tile_width;
-  let tile_height = tileset.tile_height;
-
-  let tilesheet = Texture::from_path(&mut window.factory,
-                                     format!("assets/{}", &tileset.images[0].source),
-                                     Flip::None,
-                                     &TextureSettings::new())?;
-
-  let (width, _) = tilesheet.get_size();
-  let layer = &map.layers[0];
-  let image = Image::new();
+  let tile_map = view::TileMap::new("assets/main.tmx", &mut window)?;
 
   let mut player_position = (0.0, 0.0);
 
   while let Some(e) = window.next() {
     if let Some(Button::Keyboard(key)) = e.press_args() {
       match key {
-        Key::Left => player_position.0 -= tile_width as f64 / 2.0,
-        Key::Right => player_position.0 += tile_width as f64 / 2.0,
+        Key::Left => player_position.0 -= tile_map.tile_width as f64 / 2.0,
+        Key::Right => player_position.0 += tile_map.tile_width as f64 / 2.0,
         _ => {}
       }
     }
 
     if let Some(_) = e.render_args() {
-      window.draw_2d(&e, |c, g| {
+      window.draw_2d(&e, |c, mut g| {
         clear([0.0; 4], g);
 
-        // render tile map
-        for (y, row) in layer.tiles.iter().enumerate().clone() {
-          for (x, &tile) in row.iter().enumerate() {
-            if tile == 0 {
-              continue;
-            }
-
-            let tile = tile - 1;
-
-            let src_rect = [(tile % (width / tile_width) * tile_width) as f64,
-                            (tile / (width / tile_width) * tile_height) as f64,
-                            tile_width as f64,
-                            tile_height as f64];
-
-            let trans = c.transform
-              .scale(0.5, 0.5)
-              .trans(x as f64 * tile_width as f64, y as f64 * tile_height as f64);
-
-            image.src_rect(src_rect).draw(&tilesheet, &DrawState::default(), trans, g);
-          }
-        }
+        tile_map.render(&c, &mut g);
 
         // render player
         rectangle([1.0, 0.0, 0.0, 1.0],
@@ -79,4 +47,8 @@ fn run() -> errors::Result<()> {
   Ok(())
 }
 
-quick_main!(run);
+fn main() {
+  if let Err(e) = run() {
+    println!("{}", e);
+  }
+}
