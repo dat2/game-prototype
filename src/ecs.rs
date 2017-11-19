790ec8614edb9955dda8a522a93111536c05307f
Nicholas Dujay <nickdujay@gmail.com>
2017-11-19 18:13:02 -0500

    add nphysics; need to fix it

--- src/ecs.rs
+++ src/ecs.rs
@@ -1,8 +1,14 @@
 use failure::Error;
 use graphics::{clear, DrawState, Image, rectangle, Transformed};
-use opengl_graphics::{GlGraphics, Texture, TextureSettings};
+use na::{Point2, Translation2, Vector2};
+use ncollide::shape::{Cuboid, ShapeHandle};
+use nphysics2d::object::RigidBody;
+use nphysics2d::volumetric::Volumetric;
+use nphysics2d::world::World as NWorld;
+use opengl_graphics::{GlGraphics, OpenGL, Texture, TextureSettings};
 use piston::input::{Key, RenderArgs};
-use specs::{FetchMut, Join, ReadStorage, System, VecStorage, World, WriteStorage};
+use specs::{Dispatcher, DispatcherBuilder, Entity, Entities, Fetch, FetchMut, Join, LazyUpdate,
+            ReadStorage, System, VecStorage, World, WriteStorage};
 use std::collections::VecDeque;
 use std::fs::File;
 use std::path::{Path, PathBuf};
@@ -10,24 +16,24 @@ use std::sync::Arc;
 use tiled;
 
 // components
-#[derive(Component)]
+#[derive(Component, Debug)]
 #[component(VecStorage)]
-pub struct Position {
-  pub x: f64,
-  pub y: f64,
+struct Position {
+  x: f64,
+  y: f64,
+}
+
+#[derive(Component, Debug)]
+#[component(VecStorage)]
+struct RenderRect {
+  width: f64,
+  height: f64,
+  colour: [f32; 4],
 }
 
 #[derive(Component)]
 #[component(VecStorage)]
-pub struct RenderRect {
-  pub width: f64,
-  pub height: f64,
-  pub colour: [f32; 4],
-}
-
-#[derive(Component)]
-#[component(VecStorage)]
-pub struct Tile {
+struct Tile {
   x: f64,
   y: f64,
   width: f64,
@@ -35,9 +41,38 @@ pub struct Tile {
   atlas: Arc<Texture>,
 }
 
-#[derive(Component)]
+#[derive(Component, Debug)]
 #[component(VecStorage)]
-pub struct Player;
+struct Player;
+
+#[derive(Component, Debug)]
+#[component(VecStorage)]
+struct NRigidBody {
+  shape: Cuboid<Vector2<f64>>,
+  density: Option<f64>,
+  restituion: f64,
+  friction: f64,
+}
+
+pub fn create_world() -> World {
+  let mut world = World::new();
+  world.register::<Position>();
+  world.register::<Tile>();
+  world.register::<RenderRect>();
+  world.register::<Player>();
+  world.register::<NRigidBody>();
+  world.add_resource(RenderEvents::new());
+  world.add_resource(KeyPressEvents::new());
+  world
+}
+
+pub fn create_dispatcher<'a, 'b>() -> Dispatcher<'a, 'b> {
+  DispatcherBuilder::new()
+    .add(InputSys, "input", &[])
+    .add_thread_local(PhysicsSys::new())
+    .add_thread_local(RenderSys::new(GlGraphics::new(OpenGL::V3_2)))
+    .build()
+}
 
 pub fn load_tiles_into_world<P: AsRef<Path>>(path: P, world: &mut World) -> Result<(), Error> {
   let mut path_buf = PathBuf::new();
@@ -62,8 +97,8 @@ pub fn load_tiles_into_world<P: AsRef<Path>>(path: P, world: &mut World) -> Resu
     for (x, &index) in row.iter().enumerate() {
       if index != 0 {
         let (px, py) = (
-          x as f64 * tile_dimensions.0 as f64,
-          y as f64 * tile_dimensions.1 as f64,
+          x as f64,
+          y as f64,
         );
         let (sx, sy) = (
           (((index - 1) as usize % num_tiles_per_row) * tile_dimensions.0) as f64,
@@ -79,6 +114,12 @@ pub fn load_tiles_into_world<P: AsRef<Path>>(path: P, world: &mut World) -> Resu
             height: tile_dimensions.1 as f64,
             atlas: Arc::clone(&tilesheet),
           })
+          .with(NRigidBody {
+            shape: Cuboid::new(Vector2::new(px, py)),
+            density: None,
+            restituion: 0.3,
+            friction: 0.6,
+          })
           .build();
       }
     }
@@ -90,13 +131,19 @@ pub fn load_tiles_into_world<P: AsRef<Path>>(path: P, world: &mut World) -> Resu
 pub fn create_player(world: &mut World) {
   world
     .create_entity()
-    .with(Position { x: 0.0, y: 0.0 })
+    .with(Position { x: 15.0, y: 8.0 })
     .with(RenderRect {
       width: 64.0,
       height: 64.0,
       colour: [1.0, 0.0, 0.0, 1.0],
     })
     .with(Player)
+    .with(NRigidBody {
+      shape: Cuboid::new(Vector2::new(64.0f64, 64.0f64)),
+      density: Some(1.0),
+      restituion: 0.3,
+      friction: 0.6,
+    })
     .build();
 }
 
@@ -128,12 +175,12 @@ impl RenderEvents {
 }
 
 // systems
-pub struct RenderSys {
+struct RenderSys {
   gl: GlGraphics,
 }
 
 impl RenderSys {
-  pub fn new(gl: GlGraphics) -> RenderSys {
+  fn new(gl: GlGraphics) -> RenderSys {
     RenderSys { gl: gl }
   }
 }
@@ -154,7 +201,7 @@ impl<'a> System<'a> for RenderSys {
         // draw tile
         let image = Image::new();
         for (pos, tile) in (&pos, &tile).join() {
-          let transform = c.transform.scale(0.5, 0.5).trans(pos.x, pos.y);
+          let transform = c.transform.scale(0.5, 0.5).trans(pos.x * 64.0, pos.y * 64.0);
 
           image
             .src_rect([tile.x, tile.y, tile.width, tile.height])
@@ -165,7 +212,7 @@ impl<'a> System<'a> for RenderSys {
         for (pos, rect) in (&pos, &rect).join() {
           rectangle(
             rect.colour,
-            [pos.x, pos.y, rect.width, rect.height],
+            [pos.x * 64.0, pos.y * 64.0, rect.width, rect.height],
             c.transform.scale(0.5, 0.5),
             g,
           );
@@ -175,7 +222,7 @@ impl<'a> System<'a> for RenderSys {
   }
 }
 
-pub struct InputSys;
+struct InputSys;
 
 impl<'a> System<'a> for InputSys {
   type SystemData = (FetchMut<'a, KeyPressEvents>,
@@ -188,12 +235,78 @@ impl<'a> System<'a> for InputSys {
     if let Some(key) = key_press_events.0.pop_front() {
       for (_, mut pos) in (&player, &mut pos).join() {
         match key {
-          Key::Left => pos.x -= 64.0,
-          Key::Right => pos.x += 64.0,
-          Key::Up => pos.y -= 64.0,
-          Key::Down => pos.y += 64.0,
+          Key::Left => pos.x -= 1.0,
+          Key::Right => pos.x += 1.0,
+          Key::Up => pos.y -= 1.0,
+          Key::Down => pos.y += 1.0,
           _ => {}
         };
+      }
+    }
+  }
+}
+
+struct PhysicsSys {
+  world: NWorld<f64>,
+}
+
+impl PhysicsSys {
+  fn new() -> PhysicsSys {
+    let mut world = NWorld::new();
+    // world.set_gravity(Vector2::new(0.0, 9.81));
+    PhysicsSys { world: world }
+  }
+}
+
+impl<'a> System<'a> for PhysicsSys {
+  type SystemData = (Fetch<'a, RenderEvents>,
+   Entities<'a>,
+   ReadStorage<'a, NRigidBody>,
+   WriteStorage<'a, Position>,
+   Fetch<'a, LazyUpdate>);
+
+  fn run(&mut self, data: Self::SystemData) {
+    let (render_events, entities, bodies, mut positions, lazy) = data;
+    if let Some(args) = render_events.0.front() {
+      let dt = args.ext_dt;
+
+      // add things to the world
+      for (entity, body, pos) in (&*entities, &bodies, &positions).join() {
+        let mut rb = RigidBody::new(
+          ShapeHandle::new(body.shape.clone()),
+          body.density.map(
+            |density| body.shape.mass_properties(density),
+          ),
+          body.restituion,
+          body.friction,
+        );
+        rb.append_translation(&Translation2::new(pos.x, pos.y));
+        rb.set_user_data(Some(Box::new(entity)));
+        lazy.remove::<NRigidBody>(entity);
+        self.world.add_rigid_body(rb);
+      }
+
+      // step
+      self.world.step(dt);
+
+      // update
+      let mut bodies_to_remove = Vec::new();
+      for body in self.world.rigid_bodies() {
+        let b = body.borrow();
+        if let Some(any) = b.user_data() {
+          if let Some(entity) = any.downcast_ref::<Entity>() {
+            match positions.get_mut(*entity) {
+              Some(position) => {
+                let p = b.position() * Point2::origin();
+                position.x = p.x;
+                position.y = p.y;
+              }
+              None => {
+                bodies_to_remove.push(body);
+              }
+            }
+          }
+        }
       }
     }
   }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate failure;
 extern crate glutin_window;
 extern crate graphics;
+extern crate nalgebra as na;
+extern crate ncollide;
+extern crate nphysics2d;
 extern crate opengl_graphics;
 extern crate piston;
 extern crate specs;
@@ -10,38 +13,25 @@ extern crate tiled;
 
 use failure::Error;
 use glutin_window::GlutinWindow;
-use opengl_graphics::{GlGraphics, OpenGL};
+use opengl_graphics::OpenGL;
 use piston::window::WindowSettings;
 use piston::event_loop::{Events, EventSettings};
 use piston::input::{Button, PressEvent, RenderEvent};
-use specs::{DispatcherBuilder, World};
 
 mod ecs;
 
 fn run() -> Result<(), Error> {
-  let opengl = OpenGL::V3_2;
-
   let mut window: GlutinWindow = WindowSettings::new("prototype", [960, 640])
-    .opengl(opengl)
+    .opengl(OpenGL::V3_2)
     .exit_on_esc(true)
     .build()
     .unwrap();
 
-  let mut world = World::new();
-  world.register::<ecs::Position>();
-  world.register::<ecs::Tile>();
-  world.register::<ecs::RenderRect>();
-  world.register::<ecs::Player>();
-  world.add_resource(ecs::RenderEvents::new());
-  world.add_resource(ecs::KeyPressEvents::new());
+  let mut world = ecs::create_world();
+  let mut dispatcher = ecs::create_dispatcher();
 
   ecs::load_tiles_into_world("main.tmx", &mut world)?;
   ecs::create_player(&mut world);
-
-  let mut dispatcher = DispatcherBuilder::new()
-    .add(ecs::InputSys, "input", &[])
-    .add_thread_local(ecs::RenderSys::new(GlGraphics::new(opengl)))
-    .build();
 
   let mut events = Events::new(EventSettings::new());
   while let Some(e) = events.next(&mut window) {
@@ -50,8 +40,10 @@ fn run() -> Result<(), Error> {
     }
     if let Some(args) = e.render_args() {
       world.write_resource::<ecs::RenderEvents>().push(args);
-      dispatcher.dispatch(&world.res);
     }
+
+    dispatcher.dispatch(&world.res);
+    world.maintain();
   }
 
   Ok(())
